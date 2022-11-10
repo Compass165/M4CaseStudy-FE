27a3028489df675fa27fd856618049ff6d64c422
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-10 16:14:01 +0700

    ok

--- trainer/trainerRS/list.js
+++ trainer/trainerRS/list.js
@@ -1,38 +1,12 @@
-successHandler()
+showListTrainer(0);
 
-// function addNewSmartPhone() {
-//     //lay du lieu
-//     let producer = $('#producer').val();
-//     let model = $('#model').val();
-//     let price = $('#price').val();
-//     let newSmartphone = {
-//         producer: producer,
-//         model: model,
-//         price: price
-//     };
-//     // goi ajax
-//     $.ajax({
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         type: "POST",
-//         data: JSON.stringify(newSmartphone),
-//         //tên API
-//         url: "/smartphones",
-//         //xử lý khi thành công
-//         success: successHandler
-//
-//     });
-//     //chặn sự kiện mặc định của thẻ
-//     event.preventDefault();
-// }
-
-function successHandler() {
+function showListTrainer(page) {
+    let next = page + 1;
+    let previous = page - 1;
     $.ajax({
         type: "GET",
         //tên API
-        url: "http://localhost:8080/trainer/page",
+        url: "http://localhost:8080/trainer/page?page=" + page,
         //xử lý khi thành công
         success: function (data) {
             // hien thi danh sach o day
@@ -48,14 +22,10 @@ function successHandler() {
                 content += getTrainer(data.content[i]);
             }
             content += '<div>\n' +
-                '  <a href="${trainer.id}/(page=${trainers.number - 1}, size=${trainers.size}, s=${keyword})}">\n' +
-                '    Previous\n' +
-                '  </a>\n' +
-                '  [[${data.number + 1}]]/[[${data.totalPages}]]\n' +
-                '  <a href="${trainer.id}/(page=${trainers.number + 1}, size=${trainers.size}, s=${keyword})}">\n' +
-                '    Next\n' +
-                '  </a>\n' +
-                '</div>'
+                '  <button onclick=showListTrainer('+previous+')>Previous</button>\n' +
+                + (data.pageable.pageNumber + 1) + '/' + data.totalPages +
+                '  <button onclick=showListTrainer('+next+')>Next</button>\n' +
+                '</div>';
             document.getElementById('trainerList').innerHTML = content;
         }
     });
@@ -67,30 +37,27 @@ function getTrainer(trainer) {
     return `<tr><td >${trainer.name}</td>
                 <td >${trainer.dateOfBirth}</td>
                 <td >${trainer.address}</td>` +
-        `<td><a class="deleteTrainer" href="${trainer.id}">View</a></td>` +
-        `<td><a class="deleteTrainer" href="${trainer.id}">Edit</a></td>` +
-        `<td><a class="deleteTrainer" href="${trainer.id}">Delete</a></td>
+                `<td><a class="action" href="../trainerRS/view.html">View</a></td>` +
+                `<td><a class="action" href="../trainerRS/edit.html">Edit</a></td>` +
+                `<td><a class="action" href="${trainer.id}" onclick="deleteById(this)">Delete</a></td>
                 </tr>`;
 }
 
-// $(document).ready(function () {
-//     //sư kiện nào thực hiện Ajax
-//     $('.deleteSmartphone').click(function (event) {
-//         //lay du lieu
-//         let a = $(this);
-//         let smartphoneId = a.attr("href");
-//         // goi ajax
-//         $.ajax({
-//             type: "DELETE",
-//             //tên API
-//             url: `/smartphones/${smartphoneId}`,
-//             //xử lý khi thành công
-//             success: function (data) {
-//                 a.parent().parent().remove();
-//             }
-//
-//         });
-//         //chặn sự kiện mặc định của thẻ
-//         event.preventDefault();
-//     });
-// })
+function deleteById(element){
+    //lay du lieu
+    let id = element.getAttribute("href");
+    // goi ajax
+    $.ajax({
+        type: "DELETE",
+        //tên API
+        url: "http://localhost:8080/trainer/" + id,
+        //xử lý khi thành công
+        success: function (data) {
+            console.log("Xoa thanh cong ");
+            showListTrainer();
+        }
+
+    });
+    //chặn sự kiện mặc định của thẻ
+    event.preventDefault();
+}
