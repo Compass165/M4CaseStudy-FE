1eabf8374a2df766f2501366dca5227d5db2847a
Compass <ndphong165@gmail.com>
2022-11-11 16:34:37 +0700

    done login

--- loginfm/ajax-login.js
+++ loginfm/ajax-login.js
@@ -2,13 +2,17 @@ let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 let token = localStorage.getItem("token");
 console.log(currentUser)
 
+// if (token == null) {
+//     window.location.href = "page-login.html";
+// }
+
 function checkLogin() {
     if (currentUser === null) {
         $('#dropdownMenuButton1').hide();
         $('#profile').hide();
     }
     else {
-        document.getElementById("admin-name").innerHTML = currentUser.username;
+        document.getElementById("name").innerHTML = currentUser.username;
         $('#dropdownMenuButton').hide();
     }
 }
@@ -16,19 +20,21 @@ function checkLogin() {
 checkLogin();
 
 function logout() {
-    localStorage.clear();
-    window.location.href = "home.html";
+    // localStorage.clear();
+    localStorage.removeItem("token");
+    localStorage.removeItem("currentUser");
+    window.location.href = "page-login.html";
 }
 
 function login() {
     let username = $('#name').val();
     let password = $('#password').val();
     if (username === "") {
-        document.getElementById("error_login").innerHTML = "username address cannot be blank !";
+        document.getElementById("error_login").innerHTML = "Tên tài khoản không được để trống !";
         return false;
     }
     if (password === "") {
-        document.getElementById("error_login").innerHTML = "Password can not be blank !";
+        document.getElementById("error_login").innerHTML = "Mật khẩu không được để trống !";
         return false;
     }
     let data = {
@@ -45,14 +51,21 @@ function login() {
         data: JSON.stringify(data),
         success: function (data) {
             if (data === undefined) {
-                document.getElementById("error_login").innerHTML = "Username or password is incorrect !"
+                document.getElementById("error_login").innerHTML = "Tài khoản hoặc mật khẩu không đúng !"
                 return false;
             } else {
                 localStorage.setItem("token", data.token);
                 localStorage.setItem("currentUser", JSON.stringify(data));
-                window.location.href = "home.html"
+                // window.location.href = "home.html"
+                if (data.roles[0].authority == "ROLES_ADMIN") {
+                    location.href = "../admin/admin-page.html";
+                } else if (data.roles[0].authority == "ROLES_TRAINER") {
+                    location.href = "../trainer/trainer-page.html";
+                } else if (data.roles[0].authority == "ROLES_PLAYER") {
+                    location.href = "../player/player-page.html";
+                } console.log(currentUser);
             }
-        }
-    });
+            }
+    })
     event.preventDefault();
 }
