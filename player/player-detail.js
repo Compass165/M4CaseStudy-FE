1ac6e47d3d092a2fb7802fae98ddc68a8ca46e7b
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-11-11 17:41:59 +0700

    doing player fe

--- player/player-detail.js
+++ player/player-detail.js
@@ -1,26 +1,66 @@
-function successHandler() {
+showPlayer();
+// let token = localStorage.getItem("token")
+function showPlayer() {
+
+     // let id = element.getAttribute("id");
     $.ajax({
-        type: "GET",
-        //tên API
-        url: "/smartphones",
-        //xử lý khi thành công
+        // headers: {
+        //     'Accept': 'application/json', 'Content-Type': 'application/json'
+        // },
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader ("Authorization", "Bearer " + token);
+        // },
+        type: "get", url: "http://localhost:8080/player/find-player-by-id/14",
         success: function (data) {
-            // hien thi danh sach o day
-            let content = '    <tr>\n' +
-                '        <td>Producer</td>\n' +
-                '        <td>Model</td>\n' +
-                '        <td>Price</td>\n' +
-                '        <td>Delete</td>\n' +
-                '    </tr>';
-            for (let i = 0; i < data.length; i++) {
-                content += getSmartphone(data[i]);
-            }
-            document.getElementById('smartphoneList').innerHTML = content;
+            let name = `<h2>` + data.name + `</h2>`
+            $('#player-name').append(name);
+
+            let dob = `<span>` + data.dateOfBirth + `</span>`
+            $('#dob').append(dob);
+
+            let address = `<span>` + data.address + `</span>`
+            $('#address').append(address);
+
+            let username = `<span>` + data.appUser.name + `</span>`
+            $('#username').append(username);
+
+            let password = `<span>` + data.appUser.password + `</span>`
+            $('#show_hide_password').append(password);
+
+            let height = `<span>` + data.height +" cm"+ `</span>`
+            $('#height').append(height);
+
+            let weight = `<span>` + data.weight + " kg"+`</span>`
+            $('#weight').append(weight);
+
+            let position = `<span>` + data.position.name + `</span>`
+            $('#position').append(position);
+
+            let status = `<span>` + data.status.status + `</span>`
+            $('#status').append(status);
+
+            let performance = `<span>` + data.performance.ranking + `</span>`
+            $('#performance').append(performance);
+
+            let profile = `<span>` + data.profile + `</span>`
+            $('#profile').append(profile);
+        }
+
+    })
+    event.preventDefault();
+}
+
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
         }
     });
-
-    function getSmartphone(smartphone) {
-        return `<tr><td >${smartphone.producer}</td><td >${smartphone.model}</td><td >${smartphone.price}</td>` +
-            `<td><a class="deleteSmartphone" href="${smartphone.id}">Delete</a></td></tr>`;
-    }
-}
+});
